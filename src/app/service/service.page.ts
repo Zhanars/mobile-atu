e30abf641726ff46398466fd86ee614106e93d42
root <a_janars@mail.ru>
2021-08-11 17:09:07 +0600

    first commit

---
+++ src/app/service/service.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams } from '@ionic/angular';
+
+@Component({
+  selector: 'app-service',
+  templateUrl: './service.page.html',
+  styleUrls: ['./service.page.scss'],
+})
+export class ServicePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'service',
+        loadChildren: () => import('../service/service.module').then(m => m.ServicePageModule)
+      },
+      {
+        path: 'document',
+        loadChildren: () => import('../document/document.module').then(m => m.DocumentPageModule)
+      },
+      {
+        path: 'info',
+        loadChildren: () => import('../info/info.module').then(m => m.InfoPageModule)
+      },
+      {
+        path: 'notification',
+        loadChildren: () => import('../notification/notification.module').then(m => m.NotificationPageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('../profile/profile.module').then(m => m.ProfilePageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/home',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+})
+export class TabsPageRoutingModule {}
