4bdf27f8d2316790ce4861b34eb156bb62507ee3
root <a_janars@mail.ru>
2021-08-25 17:39:45 +0600

    intrologin pages

---
+++ src/app/pages/register/register.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { RegisterPageRoutingModule } from './register-routing.module';
+
+import { RegisterPage } from './register.page';
+import {LoginPageRoutingModule} from "../login/login-routing.module";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RegisterPageRoutingModule,
+    ReactiveFormsModule
+  ],
+  declarations: [RegisterPage]
+})
+export class RegisterPageModule {}
