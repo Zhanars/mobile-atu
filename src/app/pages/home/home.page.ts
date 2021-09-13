19235fd9e607ece85e8be257a467a625aa149071
root <uit@atu.kz>
2021-09-13 20:24:56 +0600

    home page ts

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,8 +1,18 @@
-import { Component, Pipe, PipeTransform } from '@angular/core';
+import {Component, OnInit, Pipe, PipeTransform} from '@angular/core';
 import {AuthenticationService} from '../../services/authentication.service';
 import {Router} from '@angular/router';
 import { DomSanitizer} from '@angular/platform-browser';
-import {HOME_page_url} from '../../../environments/environment';
+import {HOME_page_url, httpOptions} from '../../../environments/environment';
+import {
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token, PushNotification, PushNotificationActionPerformed, PushNotificationToken,
+} from '@capacitor/push-notifications';
+import {SendServiceDataService} from "../../services/send-service-data.service";
+import {GenerateURLtokenService} from "../../services/generate-urltoken.service";
+import {HttpClient} from "@angular/common/http";
+
 
 @Pipe({ name: 'safe' })
 
@@ -17,11 +27,35 @@ export class SafePipe implements PipeTransform {
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
   home_page_url;
-  constructor(private authService: AuthenticationService, private router: Router, private domSanitizer : DomSanitizer) {
+  constructor(private authService: AuthenticationService, private router: Router, private domSanitizer : DomSanitizer,
+              private serviceDataService: SendServiceDataService,private http: HttpClient) {
     this.home_page_url = this.domSanitizer.bypassSecurityTrustResourceUrl(HOME_page_url);
   }
+  ngOnInit() {
+    PushNotifications.requestPermissions().then(result => {
+      if (result.receive === 'granted') {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
 
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener('registration',
+      (token: Token) => {
 
+        console.log(token.value);
+        const urlstring = 'https://3b32-37-228-66-82.ngrok.io/token/?key=' + GenerateURLtokenService.getKey();
+        this.http.post(urlstring, new URLSearchParams({'token': 'dsadsadsadsadsad'}), httpOptions).subscribe(
+          (response) => console.log(response),
+          (error) => console.log(error)
+        );
+      }
+    );
+  }
 }
+
