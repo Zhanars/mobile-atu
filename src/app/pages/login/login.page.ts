fb43570f2031ceefe0152b060affdba9365afd7f
root <a_janars@mail.ru>
2021-09-15 17:55:33 +0600

    login from web

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -8,6 +8,7 @@ import {IonAlertService} from "../../services/ion-alert.service";
 import {IonLoaderService} from "../../services/ion-loader.service";
 import {PushNotifications, Token} from "@capacitor/push-notifications";
 import {GenerateURLtokenService} from "../../services/generate-urltoken.service";
+import {Capacitor} from "@capacitor/core";
 
 @Component({
   selector: 'app-login',
@@ -35,25 +36,28 @@ export class LoginPage implements OnInit {
     });
   }
   login() {
+    const isPushNotificationsAvailable = Capacitor.isPluginAvailable('PushNotifications');
     this.ionLoaderService.customLoader();
+    if (isPushNotificationsAvailable) {
+      PushNotifications.requestPermissions().then(result => {
+        if (result.receive === 'granted') {
+          // Register with Apple / Google to receive push via APNS/FCM
+          PushNotifications.register();
+        } else {
+          ///fsdfsfsdfs
+        }
+      });
 
-    PushNotifications.requestPermissions().then(result => {
-      if (result.receive === 'granted') {
-        // Register with Apple / Google to receive push via APNS/FCM
-        PushNotifications.register();
-      } else {
-        // Show some error
-      }
-    });
+      // On success, we should be able to receive notifications
+      PushNotifications.addListener('registration',
+        (token: Token) => {
+          this.setTokenFirebase(token.value);
+        }
+      );
+    } else {
+      this.setTokenFirebase('web');
+    }
 
-    // On success, we should be able to receive notifications
-    PushNotifications.addListener('registration',
-      (token: Token) => {
-      console.log('Qwerty123456Q: '+token.value);
-        this.setTokenFirebase(token.value);
-        console.log('Qwerty123456Q123: '+this.data.token_firebase);
-      }
-    );
   }
 
   // Easy access for form fields
@@ -71,7 +75,6 @@ export class LoginPage implements OnInit {
 
   setTokenFirebase(value: string){
     this.data.token_firebase = value;
-      console.log('Qwerty123456Q1234: '+this.data.token_firebase);
       this.data.email = this.credentials.controls['email'].value;
       this.data.password = this.credentials.controls['password'].value;
       console.log(this.data);
@@ -79,20 +82,23 @@ export class LoginPage implements OnInit {
         (res: any) => {
           if (res.code == '1'){
             Storage.set({key: AUTH_TOKEN_KEY, value: JSON.stringify(res.message)});
-            this.ionLoaderService.dismissLoader();
             this.authService.isAuthenticated.next(true);
-            this.router.navigateByUrl('/tabs/home', {replaceUrl: true});
           } else {
             this.ionLoaderService.dismissLoader();
+            this.authService.isAuthenticated.next(false);
             this.ionAlertService.showAlert('Ошибка', 'Email или пароль не правильный', '');
           }
         },
         res => {
           this.ionLoaderService.dismissLoader();
+          this.authService.isAuthenticated.next(false);
           this.ionAlertService.showAlert('Ошибка', 'Сервер недоступен, попробуйте позже', '');
         }
       );
-      this.ionLoaderService.dismissLoader();
+    this.ionLoaderService.dismissLoader();
+    if (this.authService.isAuthenticated){
+      this.router.navigateByUrl('/tabs', { replaceUrl: true });
+    }
   }
 
 
