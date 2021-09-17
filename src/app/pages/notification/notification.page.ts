11c212f1e3f666a73df430aab8ee7bb7f6ea02ae
Murat <a.zhanars@gmail.ru>
2021-09-17 09:21:33 +0600

    krutoi poisk

--- src/app/pages/notification/notification.page.ts
+++ src/app/pages/notification/notification.page.ts
@@ -1,16 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { PopoverController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.page.html',
   styleUrls: ['./notification.page.scss'],
 })
 export class NotificationPage implements OnInit {
-
-  constructor( ) {
+  emails = [];
+  constructor(private http: HttpClient, private popoverCtrl: PopoverController, private router: Router) {
   }
 
-  ngOnInit() {
+  ngOnInit() { this.http.get<any[]>('https://devdactic.fra1.digitaloceanspaces.com/gmail/data.json').subscribe(res => {
+    this.emails = res;
+    for (let e of this.emails) {
+      // Create a custom color for every email
+      e.color = this.intToRGB(this.hashCode(e.from));
+    }
+  });
   }
 
+  openDetails(id) {
+    this.router.navigate(['tabs', 'mail', id]);
+  }
+
+  // https://stackoverflow.com/questions/3426404/create-a-hexadecimal-colour-based-on-a-string-with-javascript
+  private hashCode(str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+  }
+
+  private intToRGB(i) {
+    const c = (i & 0x00FFFFFF)
+      .toString(16)
+      .toUpperCase();
+
+    return '#' + '00000'.substring(0, 6 - c.length) + c;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  doRefresh(ev) {
+    setTimeout(() => {
+      ev.target.complete();
+    }, 2000);
+  }
 }
