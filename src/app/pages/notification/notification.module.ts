ee48487d70159067c9368c62ed10d560d178b32f
root <uit@atu.kz>
2021-09-20 16:48:00 +0600

    notification

--- src/app/pages/notification/notification.module.ts
+++ src/app/pages/notification/notification.module.ts
@@ -13,7 +13,9 @@ import { NotificationPage } from './notification.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    NotificationPageRoutingModule
+    NotificationPageRoutingModule,
+    CommonModule,
+    CommonModule
   ],
   declarations: [NotificationPage]
 })
